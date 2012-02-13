e67c871c547317cec4f1fc3633f5e9738866f16d
vincent.blouin@gmail.com <vincent.blouin@gmail.com@40667f71-9be7-da36-2ea2-2722c72b7549>
2012-02-13 03:24:22 +0000

    refactor

--- src/test/java/org/triple_brain/repository_sql/SQLUserRepositoryTest.java
+++ src/test/java/org/triple_brain/repository_sql/SQLUserRepositoryTest.java
@@ -3,11 +3,15 @@ package org.triple_brain.repository_sql;
 import com.mycila.jdbc.query.Results;
 import org.junit.Test;
 import org.triple_brain.model.User;
+import org.triple_brain.module.repository.user.user.ExistingUserException;
 
 import javax.inject.Inject;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
 
 /**
  * @author Vincent Blouin
@@ -26,6 +30,28 @@ public class SQLUserRepositoryTest extends AbstractSqlTest{
 
         result = sql.query("SELECT id, uuid FROM por_user").execute();
         assertThat(result.rows.size(), is(1));
+    }
+
+    @Test
+    public void try_to_save_twice_a_user_with_same_email_is_not_possible() {
+        User user_1 = User.withEmail("roger.lamothe@me.com");
+        User user_2 = User.withEmail("roger.lamothe@me.com");
+
+        assertThat(users().size(), is(0));
+
+        repository.save(user_1);
+
+        try {
+            repository.save(user_2);
+            fail();
+        } catch (ExistingUserException e) {
+            assertThat(e.getMessage(), is("A user already exist with email: roger.lamothe@me.com"));
+        }
+    }
+
+    final List<User> users() {
+        return sql.query("SELECT * FROM por_user")
+                .list(User.class);
     }
     
 }
