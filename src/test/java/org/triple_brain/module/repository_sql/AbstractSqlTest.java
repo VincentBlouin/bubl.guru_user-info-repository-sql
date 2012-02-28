fbd6534a2983979437b66f1d4fa1777fb279afec
vincent.blouin@gmail.com <vincent.blouin@gmail.com@40667f71-9be7-da36-2ea2-2722c72b7549>
2012-02-28 03:57:42 +0000

    create sql test for user repository

---
+++ src/test/java/org/triple_brain/module/repository_sql/AbstractSqlTest.java
@@ -0,0 +1,61 @@
+package org.triple_brain.module.repository_sql;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Stage;
+import com.google.inject.util.Modules;
+import com.mycila.inject.jsr250.Jsr250;
+import org.junit.After;
+import org.junit.Before;
+import org.triple_brain.module.repository.user.user.UserRepository;
+
+import java.sql.SQLException;
+
+import static org.triple_brain.module.repository_sql.SQLConnection.closeConnection;
+import static org.triple_brain.module.repository_sql.SQLConnection.preparedStatement;
+
+/**
+ * @author Vincent Blouin
+ */
+public class AbstractSqlTest implements Module {
+
+    @Before
+    public final void before() throws SQLException {
+        Jsr250.createInjector(Stage.PRODUCTION, Modules.override(new SQLModule()).with(this)).injectMembers(this);
+        cleanTables();
+    }
+
+    protected void cleanTables()throws SQLException{
+        String query = "DROP TABLE IF EXISTS por_user;";
+        preparedStatement(query).executeUpdate();
+        createTables();
+    }
+
+    protected void createTables() throws SQLException{
+        String query = "CREATE TABLE por_user (\n" +
+                "    id           BIGINT    PRIMARY KEY AUTO_INCREMENT,\n" +
+                "    creationTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\n" +
+                "    updateTime   TIMESTAMP NOT NULL,\n" +
+                "\n" +
+                "    uuid   VARCHAR(36)   UNIQUE NOT NULL,\n" +
+                "    email  VARCHAR(50)   UNIQUE NOT NULL,\n" +
+                "    locale VARCHAR(10)   NOT NULL DEFAULT 'en_US',\n" +
+                "\n" +
+                "    salt                 VARCHAR(36),\n" +
+                "    passwordHash         VARCHAR(100),\n" +
+                "    firstname            VARCHAR(50),\n" +
+                "    lastname             VARCHAR(50)\n" +
+                ");";
+        preparedStatement(query).executeUpdate();
+    }
+
+    @After
+    public final void after() throws SQLException {
+        closeConnection();
+    }
+
+    @Override
+    public final void configure(Binder binder) {
+        binder.bind(UserRepository.class).to(SQLUserRepository.class);
+    }
+}
---
+++ src/test/java/org/triple_brain/module/repository_sql/SQLUserRepositoryTest.java
@@ -0,0 +1,81 @@
+package org.triple_brain.module.repository_sql;
+
+import org.junit.Test;
+import org.triple_brain.module.model.User;
+import org.triple_brain.module.repository.user.user.ExistingUserException;
+
+import javax.inject.Inject;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.triple_brain.module.repository_sql.SQLConnection.preparedStatement;
+
+/**
+ * @author Vincent Blouin
+ */
+public class SQLUserRepositoryTest extends AbstractSqlTest{
+
+    @Inject
+    SQLUserRepository userRepository;
+
+    @Test
+    public void can_save_user() throws Exception{
+        ResultSet resultSet = preparedStatement("SELECT id, uuid FROM por_user").executeQuery();
+        assertFalse(resultSet.next());
+
+        User user = User.withEmail("roger.lamothe@me.com").password("patate");
+        userRepository.save(user);
+
+        resultSet = preparedStatement("SELECT id, uuid FROM por_user").executeQuery();
+        assertTrue(resultSet.next());
+    }
+
+    @Test
+    public void try_to_save_twice_a_user_with_same_email_is_not_possible() throws Exception{
+        User user_1 = User.withEmail("roger.lamothe@me.com");
+        User user_2 = User.withEmail("roger.lamothe@me.com");
+
+        assertThat(users().size(), is(0));
+
+        userRepository.save(user_1);
+
+        try {
+            userRepository.save(user_2);
+            fail();
+        } catch (ExistingUserException e) {
+            assertThat(e.getMessage(), is("A user already exist with email: roger.lamothe@me.com"));
+        }
+    }
+
+    @Test
+    public void user_fields_are_well_saved() {
+        User user = User.withEmail("roger@me.com")
+                .firstName("Roger").lastName("Lamothe")
+                .password("secret");
+        userRepository.save(user);
+
+        User loadedUser = userRepository.findByEmail("roger@me.com");
+
+        assertThat(loadedUser.id(), is(user.id()));
+        assertThat(loadedUser.email(), is(user.email()));
+        assertThat(userRepository.getInternalId(loadedUser), is(userRepository.getInternalId(user)));
+
+        assertThat(loadedUser.firstName(), is(user.firstName()));
+        assertThat(loadedUser.lastName(), is(user.lastName()));
+        assertTrue(loadedUser.hasPassword("secret"));
+    }
+
+    final List<User> users() throws Exception{
+        ResultSet resultSet = preparedStatement("SELECT * FROM por_user").executeQuery();
+        List<User> users = new ArrayList<User>();
+        while(resultSet.next()){
+            users.add(userRepository.userFromResultSet(resultSet));
+        }
+        return users;
+    }
+
+}
